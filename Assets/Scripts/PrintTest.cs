bb9b7d64724e8025eae5356e65869799ba3415c6
Aviv5374 <Aviv5374@gmail.com>
2019-09-25 12:47:20 +0300

    Test Commit

--- Assets/Scripts/PrintTest.cs
+++ Assets/Scripts/PrintTest.cs
@@ -7,7 +7,7 @@ public class PrintTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TEST!!!!!");
+        Debug.LogWarning("TEST!!!!!");
     }
 
     // Update is called once per frame
