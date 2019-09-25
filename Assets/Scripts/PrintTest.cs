0caf514c2ec13f28761bd32f69246f51a268909a
Aviv5374 <Aviv5374@gmail.com>
2019-09-25 13:04:27 +0300

    Make Simple Tree different From Test Branch

--- Assets/Scripts/PrintTest.cs
+++ Assets/Scripts/PrintTest.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PrintTest : MonoBehaviour
 {
+    int num;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("TEST!!!!!");
+        num = 5;
     }
 
     // Update is called once per frame
